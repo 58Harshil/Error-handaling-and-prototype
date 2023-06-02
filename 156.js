8f676e12279959c873aff21d10d1360bffe2ca71
Harshil Choudhary <112849807+58Harshil@users.noreply.github.com>
2023-06-02 22:51:48 +0530

    Create 156.js

---
+++ 156.js
@@ -0,0 +1,28 @@
+class Product{
+    name;
+    price;
+    rating;
+
+    constructor(n,p,r){
+        console.log("Calling the constructor");
+        this.name= n;
+        this.price =p;
+        this.rating=r;
+        //return 10
+    }
+
+    display(){
+        //console.log(this);
+        console.log("Displaying the current product");
+    }
+}
+
+const p = new Product('Iphone',10000,5);
+console.log(p);
+//p.display();
+
+let x= {
+    p: Product
+};
+x.p("Airpods",1500,6)
+console.log(x);
